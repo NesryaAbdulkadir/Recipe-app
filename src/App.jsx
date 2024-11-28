137d48c844a3b39131d1e2df6c394c9467a4bef2
NesryaAbdulkadir <nesryaabdulkadir.dev@gmail.com>
2024-11-28 19:46:00 +0300

    Recipe app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import AllRecipies from "./components/AllRecipies";
+
+export default function App() {
+  return (
+    <div>
+      <AllRecipies />
+    </div>
+  );
+}
---
+++ src/components/AllRecipies/index.jsx
@@ -0,0 +1,138 @@
+import { Youtube } from "lucide-react";
+import React, { useEffect, useState } from "react";
+
+export default function AllRecipies() {
+  const [recipes, setRecipes] = useState([]);
+
+  async function getRecipes() {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+    );
+    const data = await response.json();
+    if (data.meals) {
+      const structuredMeals = data.meals.map((meal) => ({
+        id: meal.idMeal,
+        meal: meal.strMeal,
+        ingredients: [
+          {
+            ingredient: meal.strIngredient1,
+            measurement: meal.strMeasure1,
+          },
+          {
+            ingredient: meal.strIngredient2,
+            measurement: meal.strMeasure2,
+          },
+          {
+            ingredient: meal.strIngredient3,
+            measurement: meal.strMeasure3,
+          },
+          {
+            ingredient: meal.strIngredient4,
+            measurement: meal.strMeasure4,
+          },
+          {
+            ingredient: meal.strIngredient5,
+            measurement: meal.strMeasure5,
+          },
+          {
+            ingredient: meal.strIngredient6,
+            measurement: meal.strMeasure6,
+          },
+          {
+            ingredient: meal.strIngredient7,
+            measurement: meal.strMeasure7,
+          },
+          {
+            ingredient: meal.strIngredient8,
+            measurement: meal.strMeasure8,
+          },
+          {
+            ingredient: meal.strIngredient9,
+            measurement: meal.strMeasure9,
+          },
+        ],
+        catagory: meal.strCategory,
+        instructions: meal.strInstructions,
+        tags: meal.strTags?.split(","),
+        image: meal.strMealThumb,
+        youtube: meal.strYoutube,
+      }));
+      setRecipes(structuredMeals);
+    }
+  }
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-center mt-5">Recipes to try</h1>
+      <ul className=" p-10 flex flex-col gap-5 ">
+        {recipes?.map((recipe) => (
+          <li
+            className=" grid grid-cols-1 lg:grid-cols-2 gap-8"
+            key={recipe.id}
+          >
+            <div className="relative">
+              <p className="absolute top-0 left-0 bg-pink-100  px-4 py-2  rounded-b-xl">
+                {recipe.catagory}
+              </p>
+              <img
+                src={recipe.image}
+                alt={recipe.meal}
+                className="h-full w-full object-cover col-span-1"
+              />
+              {recipe?.youtube && (
+                <a
+                  href={recipe.youtube}
+                  className="absolute bottom-0 left-0 "
+                  alt="watch on youtube"
+                  target="_blank"
+                >
+                  <Youtube className="w-12 h-12 text-white bg-red-500 px-2 py-1 rounded-t-full" />
+                </a>
+              )}
+            </div>
+            <div className="pt-4 flex flex-col gap-3 col-span-1">
+              <h2>{recipe?.meal}</h2>
+              <p className="flex flex-wrap gap-2">
+                {recipe?.tags?.map((tag, index) => (
+                  <span
+                    key={index}
+                    className={`py-2 px-4 rounded-full ${
+                      index === 0
+                        ? "bg-pink-100"
+                        : index === 1
+                        ? "bg-blue-100"
+                        : index === 2
+                        ? "bg-green-100"
+                        : index === 3
+                        ? "bg-yellow-100"
+                        : "bg-gray-100" // Default color for tags beyond the first four
+                    }`}
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </p>
+              <h2>Ingredients</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-y-0 ">
+                {recipe?.ingredients.map((ingredient, index) => (
+                  <div key={index} className=" flex gap-3">
+                    <span>{ingredient.measurement}</span>
+                    <span>{ingredient.ingredient}</span>
+                  </div>
+                ))}
+              </div>
+              <h2>Instructions</h2>
+              {recipe?.instructions && (
+                <p className="">{recipe?.instructions}</p>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
